1536aebe24f9752a2c8b41ae388bebff38a9365f
Karthikeyan K <80576697+triplek24@users.noreply.github.com>
2022-02-26 22:11:40 +0530

    //circle oops

---
+++ circle.js
@@ -0,0 +1,41 @@
+class Circle {
+    constructor(...args) //radius=1.0, color="Red")
+        {
+            if (args.length == 0) {
+
+            } else if (args.length == 1) {
+                this.radius = args[0];
+            } else if (args.length == 2) {
+                this.radius = args[0];
+                this.color = args[1];
+            }
+        }
+
+    getRadius() {
+        return this.radius;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    setRadius(num) {
+        this.radius = num;
+    }
+
+    setColor(col) {
+        this.color = col;
+    }
+
+    getArea() {
+        return Math.pow(this.radius, 2).toFixed(2);
+    }
+
+}
+
+
+var circle = new Circle(5.4);
+
+
+console.log(circle.getArea());
+console.log(circle.getColor());
